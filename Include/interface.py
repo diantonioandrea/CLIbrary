9b59d8c34759ad5578820d8d31281a981c69f7c8
Andrea Di Antonio <82906478+diantonioandrea@users.noreply.github.com>
2022-10-30 13:21:08 +0100

    Update

--- Include/interface.py
+++ Include/interface.py
@@ -1,11 +1,16 @@
+from colorama import init, Fore
+init()
+
 # COMMANDS HANDLING
 
 def getCommand(commandString: str):
+	errorString = ""
+
 	while True:
 		try: 
 
-			command = " ".join(input(commandString).split()).lower()
-			instructions = command.split(" ")
+			commandString = " ".join(input(errorString + commandString).split()).lower()
+			instructions = commandString.split(" ")
 
 			# OPTIONS: SINGLE DASH [[-key1, value1], ...] AND DOUBLE DASH [--key1, ...]
 
@@ -14,6 +19,10 @@ def getCommand(commandString: str):
 
 			if "_" not in instructions[0]:
 				command = instructions[0]
+
+			else:
+				errorString = Fore.RED + "\nSYNTAX ERROR " + Fore.RESET
+				continue
 
 			for inst in instructions:
 				if "--" in inst:
@@ -27,7 +36,12 @@ def getCommand(commandString: str):
 					except(ValueError):
 						sdOpts.append([inst, instructions[instructions.index(inst) + 1]])
 		
-		except(IndexError, EOFError, KeyboardInterrupt):
+		except(IndexError):
+			errorString = Fore.RED + "\nSYNTAX ERROR " + Fore.RESET
+			continue
+
+		except(EOFError, KeyboardInterrupt):
+			errorString = Fore.RED + "\nKEYBOARD ERROR " + Fore.RESET
 			continue
 			
 		return command, sdOpts, ddOpts
---
+++ REAMDE.md
@@ -0,0 +1,9 @@
+# CLIbrary
+
+A standardized collection of CLI utilities written in Python to handle commands, I/O and files.
+
+## Purposes
+
+### Standardization
+
+CLIbrary was created to have a way of standardize all my projects no matter their importance.
